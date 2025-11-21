a49551b9f3fce791bc4877e9ae2026923a086e91
Alyssa Leliveld <alyssaleliveld@outlook.com>
2025-11-21 17:00:13 +0100

    Working on header

---
+++ web/header.php
@@ -0,0 +1,31 @@
+<link href="../css/header.css" rel ="stylesheet">
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<header>
+<nav class="navbar navbar-expand-sm bg-body-tertiary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="homepage.php">Tappers & Trimmers</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="homepage.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">Over ons</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="bar.php">Bar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="shop.php">Shop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="reservation.php">Reserveren</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
--- web/homepage.php
+++ web/homepage.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+    include("header.php");
+    ?>
 </head>
 <body>
-    
+    <?php
+    include("footer.php");
+    ?>
 </body>
 </html>
