ec32d85f732b602983a74364e26349eb04ae0b25
Alyssa Leliveld <alyssaleliveld@outlook.com>
2025-11-28 10:22:31 +0100

    Header afgemaakt

--- web/header.php
+++ web/header.php
@@ -1,30 +1,29 @@
-<link href="../css/header.css" rel ="stylesheet">
-<link href="../css/bootstrap.min.css" rel="stylesheet">
 <header>
-<nav class="navbar navbar-expand-sm bg-body-tertiary" data-bs-theme="dark">
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<link href="../css/header.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+
+<nav class="navbar navbar-expand-lg custom-navbar">
   <div class="container-fluid">
-    <a class="navbar-brand" href="homepage.php">Tappers & Trimmers</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="homepage.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about.php">Over ons</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="bar.php">Bar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="shop.php">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="reservation.php">Reserveren</a>
-        </li>
-      </ul>
+    <a class="navbar-brand d-flex align-items-center" href="homepage.php">
+      <img src="../assets/logo.png" alt="logo" class="logo me-2"/>
+      Tappers & Trimmers
+    </a>
+     <ul class="nav-links navbar-nav ms-auto" id="navLinks">
+            <li class="nav-item"><a class="nav-link" href="homepage.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="about.php">Over ons</a></li>
+            <li class="nav-item"><a class="nav-link" href="bar.php">Bar</a></li>
+            <li class="nav-item"><a class="nav-link" href="Shop.php">Shop</a></li>
+            <li class="nav-item d-flex align-items-center">
+                <a href="cart.php" class="nav-link p-0 me-2">
+                    <button class="cart-btn" id="cartBtn">
+                        <i class="bi bi-cart"></i>
+                        <span id="cartCount">0</span>
+                    </button>
+                </a>
+                <button class="reservation ms-2" onclick="location.href='reservation.php'">Reserveren</button>
+            </li>
+        </ul>
     </div>
   </div>
 </nav>
