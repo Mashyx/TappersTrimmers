5bdbac45ff6565d95b220c491dc4a3999f4663cf
Mikevdrg <154432@student.aventus.nl>
2025-12-05 11:59:47 +0100

    logo

--- web/homepage.php
+++ web/homepage.php
@@ -12,14 +12,17 @@
     <?php
     include("header.php");
     ?>
-    <!-- fotos voor de homepage -->
-    <div class="afbeeldingHome">
-        <img src="../assets/pngtree-barbershop-with-chairs-in-traditional-styles-and-mirrors-in-front-picture-image_3372710.jpg" width="900" height="400">
-        <img src="../assets/SFD_Minimalism_1_Jaguar_Sun_CR_Adam_DelGiudice_2520x1420.jpg" width="900" height="400">
-    </div>
+    <div class="afbeeldingHome hero">
+        <div class="hero-left">
+            <h1 class="site-title">Tappers & Trimmers</h1>
+            <p class="lead">Welkom bij onze kapperszaak! Wij bieden professionele knip- en trimdiensten voor mannen en kinderen vanaf 12 jaar. Onze ervaren kappers zorgen ervoor dat je er altijd op je best uitziet. Kom langs voor een stijlvolle knipbeurt of een verzorgende trimbeurt.</p>
+        </div>
 
-    <div class="tekstMidden">
-        <p>Welkom bij onze kapperszaak! Wij bieden professionele knip- en trimdiensten voor mannen en kinderen vanaf 12 jaar!. Onze ervaren kappers zorgen ervoor dat je er altijd op je best uitziet. Kom langs voor een stijlvolle knipbeurt of een verzorgende trimbeurt. We kijken ernaar uit je te verwelkomen!</p>
+        <div class="hero-right">
+            <div class="logo-wrap">
+                <img src="../assets/logodesign.png" alt="Tappers & Trimmers logo" class="logo-img">
+            </div>
+        </div>
     </div>
 
     <!-- Prijslijst kapper -->
