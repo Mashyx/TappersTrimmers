9cd0db0a430e090e5863c874de1e661ff981b9e3
Jelle-490 <wiertzjelle@gmail.com>
2025-12-12 11:58:46 +0100

    css aangemaakt+ table

--- web/reservation.php
+++ web/reservation.php
@@ -3,14 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reservation.css">
+
     <title>Document</title>
       <?php
     include("header.php");
     ?>
 </head>
-<body>
-    
+<body class="body">
+    <h1 class='h1'>reservering</h1>
 
+  <tabel>
+    <tr>
+      <th>test</th>
+    </tr>
+  </table>
 
 
 <?php
