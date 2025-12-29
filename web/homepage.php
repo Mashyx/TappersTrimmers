b13bf9569929987d5c70e3506cb855c05c5298e6
Mikevdrg <154432@student.aventus.nl>
2025-12-29 12:46:34 +0100

    update 29-12

--- web/homepage.php
+++ web/homepage.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="../css/style.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home pagina</title>
+    <link href="../css/style.css" rel="stylesheet">
     <?php
     include("header.php");
     ?>
@@ -12,14 +12,9 @@
 <body>
      <!-- fotos voor de homepage -->
     <div class="afbeeldingHome">
-        <img src="../assets/pngtree-barbershop-with-chairs-in-traditional-styles-and-mirrors-in-front-picture-image_3372710.jpg" width = "900" height = "400">
-        <img src="../assets/SFD_Minimalism_1_Jaguar_Sun_CR_Adam_DelGiudice_2520x1420.jpg" width = "900" height = "400">
-    </div>
-
-    <div class="tekstMidden">
+        <img src="../assets/Untitled design.png" alt="logo" class="logo me-2"/>
         <p>Welkom bij onze kapperszaak! Wij bieden professionele knip- en trimdiensten voor mannen en kinderen vanaf 12 jaar!. Onze ervaren kappers zorgen ervoor dat je er altijd op je best uitziet. Kom langs voor een stijlvolle knipbeurt of een verzorgende trimbeurt. We kijken ernaar uit je te verwelkomen!</p>
     </div>
-    <!-- fotos voor de homepage -->
 
 
     <?php
