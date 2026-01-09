a3f7e0d9cefa27c0c15c7a3337afd41b581b8b67
Jelle-490 <wiertzjelle@gmail.com>
2026-01-09 10:57:04 +0100

    bar aan gemaakt
    ben nog bezig maar niet compleet af moet nog hoofd letters en het img aanpassen

--- web/bar.php
+++ web/bar.php
@@ -3,30 +3,84 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bar.css">
+
     <title>Document</title>
       <?php
     include("header.php");
     ?>
-    <title>Home pagina</title>
-    <link rel="stylesheet" href="../css/style.css">
-
-
 </head>
 <body class="body">
-    <!-- fotos voor de homepage -->
-     <div class="afbeeldingHome">
-    <img src="../assets/pngtree-barbershop-with-chairs-in-traditional-styles-and-mirrors-in-front-picture-image_3372710.jpg" width = "900" height = "400">
-    <img src="../assets/SFD_Minimalism_1_Jaguar_Sun_CR_Adam_DelGiudice_2520x1420.jpg" width = "900" height = "400">
+     <img src="../assets/istockphoto-618862242-2048x2048.jpg" height="50%" width="100%"  id="aldceholIMG">
+    <h1 class="txt" class='h1'>dranken</h1>
+    <p class="txt_w">Op vrijdag is er een speciaal aanbieding! van 16:00 tot 18:00 is er happy hour. 
+    Tijdens happy hour zijn alle frisdranken en bieren voor de helft van de prijs! Kom dus snel langs 
+    en geniet van onze heerlijke dranken tegen een gereduceerde prijs!
+    </p>
+   
+      <div id="dranken" class="txt">
+
+    <div id= "frisdranken">
+        <h1 class="txt_w">frisdranken</h1>
+        <h2>cola €3,20 </h2>
+        <p class="txt_w">een cola van 0,40ml</p>
+
+        <h2>fanta €3,20</h2>
+        <p class="txt_w">een fanta van 0,40ml</p>
+
+        <h2>7-up €3,20</h2>
+        <p class="txt_w">een 7-up van 0,40ml</p>
+
+        <h2>icetea €3,20</h2>
+        <p class="txt_w">een icetea van 0,40ml</p>
+        </div>
+            
+
+    <div id="wijnen">
+        <h1 class="txt_w">wijen</h1>
+        <h2>rode wijn €4,50</h2>
+        <p  class="txt_w">een glas rode wijn van 0,25ml</p>
+
+        <h2>witte wijn €4,50</h2>
+        <p class="txt_w">een glas witte wijn van 0,25ml</p>
+
+        <h2>rosé wijn €4,50</h2>
+        <p class="txt_w">een glas rosé wijn van 0,25ml</p>
     </div>
 
-    <div class="tekstMidden">
-    <p>Welkom bij onze kapperszaak! Wij bieden professionele knip- en trimdiensten voor mannen en kinderen vanaf 12 jaar!. Onze ervaren kappers zorgen ervoor dat je er altijd op je best uitziet. Kom langs voor een stijlvolle knipbeurt of een verzorgende trimbeurt. We kijken ernaar uit je te verwelkomen!</p>
-</div>
-    <!-- fotos voor de homepage -->
 
-        <?php
+    <div id="bieren">
+        <h2 class="txt_w">bieren</h2>
+        <h2>heineken €3,50</h2>
+        <p class="txt_w">een heineken van 0,40ml</p>
+
+        <h2>grolsch €3,50</h2>
+        <p class="txt_w">een grolsch van 0,40ml</p>
+
+        <h2>stella artois €3,50</h2>
+        <p class="txt_w">een stella artois van 0,40ml</p>
+    </div>
+
+    <div id="cocktails">
+        <h2 class="txt_w">cocktails €7,50</h2>
+        <h2>mojito</h2>
+        <p class="txt_w">een verfrissende cocktail met rum, munt, limoen en suiker</p>
+        <h2>pina colada</h2>
+        <p class="txt_w">een tropische cocktail met rum, kokos en ananas</p>
+        <h2>sex on the beach</h2>
+        <p class="txt_w">een zoete cocktail met vodka, perziklikeur, sinaasappel- en cranberrysap</p>
+    </div>
+
+
+      </div>
+
+
+
+    
+  
+
+<?php
     include("footer.php");
     ?>
-
 </body>
 </html>
