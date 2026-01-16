56c016a0c09575d6f11f30c77e9bfe934203787f
Jelle-490 <wiertzjelle@gmail.com>
2026-01-16 11:55:29 +0100

    reservations

--- web/reservation.php
+++ web/reservation.php
@@ -10,56 +10,7 @@
     include("header.php");
     ?>
 </head>
-<body class="body">
-    <h1 class="txt" class='h1'>reservering</h1>
-  
-  <div id="reservering_div_kapper">
-  <table class=txt id="reservering_table_kapper">
-    <tr>
-      <th>maandag</th>
-      <th>dinsdag</th>
-      <th>woensdag</th>
-      <th>donderdag</th>
-      <th>vrijdag</th>
-      <th>zaterdag</th>
-      <th>zondag</th>
-    </tr>
-    <br>
-    <tr>
-      <td>gesloten</td>
-      <td>09:00-18:00</td>
-      <td>09:00-18:00</td>
-      <td>09:00-20:00</td>
-      <td>09:00-18:00</td>
-      <td>10:00-18:00</td>
-      <td>gesloten</td>
-    </tr>
-  </table>
-  </div>
 
-  <div id="reservering_div_bar">
-    <table id="reservering_table_bar" class="txt">
-      <tr>
-        <th>maandag</th>
-        <th>dinsdag</th>
-        <th>woensdag</th>
-        <th>donderdag</th>
-        <th>vrijdag</th>
-        <th>zaterdag</th>
-        <th>zondag</th>
-      </tr>
-      <br>
-      <tr>
-        <td>gesloten</td>
-        <td>12:00-20:00</td>
-        <td>12:00-20:00</td>
-        <td>12:00-20:00</td>
-        <td>12:00-20:00</td>
-        <td>12:00-20:00</td>
-        <td>gesloten</td>
-      </tr>
-    </table>
-    </div>
 <?php
     include("footer.php");
     ?>
