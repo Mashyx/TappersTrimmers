7ab643176fdf00afc3dfe1017a914a0222f07ab0
Jelle-490 <wiertzjelle@gmail.com>
2026-01-16 11:57:53 +0100

    reservations terugdraaien

--- web/reservation.php
+++ web/reservation.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
     <title>Document</title>
       <?php
     include("header.php");
@@ -12,6 +10,7 @@
 </head>
 <body>
     
+
 
 
 <?php
