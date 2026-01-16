c253e49732968c96e0a763decf55c0be2a7027fb
Jelle-490 <wiertzjelle@gmail.com>
2026-01-16 10:39:41 +0100

    hoofdlettes aangepast ben bezig met img
    fas

--- web/bar.php
+++ web/bar.php
@@ -11,7 +11,12 @@
     ?>
 </head>
 <body class="body">
-     <img src="../assets/istockphoto-618862242-2048x2048.jpg" height="50%" width="100%"  id="aldceholIMG">
+    <div id="test"><h1>test</h1></div>
+
+    <div id="bar_mainIMG" class="mainIMG">
+     <img src="../assets/alcehol_img3.jpeg" height="20%" width="80%"  id="aldceholIMG" >
+    </div>
+
     <h1 class="txt" class='h1'>dranken</h1>
     <p class="txt_w">Op vrijdag is er een speciaal aanbieding! van 16:00 tot 18:00 is er happy hour. 
     Tijdens happy hour zijn alle frisdranken en bieren voor de helft van de prijs! Kom dus snel langs 
@@ -21,54 +26,54 @@
       <div id="dranken" class="txt">
 
     <div id= "frisdranken">
-        <h1 class="txt_w">frisdranken</h1>
-        <h2>cola €3,20 </h2>
-        <p class="txt_w">een cola van 0,40ml</p>
+        <h1 class="txt_w">Frisdranken</h1>
+        <h2>Cola €3,20 </h2>
+        <p class="txt_w">Een cola van 0,40ml</p>
 
-        <h2>fanta €3,20</h2>
-        <p class="txt_w">een fanta van 0,40ml</p>
+        <h2>Fanta €3,20</h2>
+        <p class="txt_w">Een fanta van 0,40ml</p>
 
         <h2>7-up €3,20</h2>
-        <p class="txt_w">een 7-up van 0,40ml</p>
+        <p class="txt_w">Een 7-up van 0,40ml</p>
 
-        <h2>icetea €3,20</h2>
-        <p class="txt_w">een icetea van 0,40ml</p>
+        <h2>Icetea €3,20</h2>
+        <p class="txt_w">Een icetea van 0,40ml</p>
         </div>
             
 
     <div id="wijnen">
-        <h1 class="txt_w">wijen</h1>
+        <h1 class="txt_w">Wijen</h1>
         <h2>rode wijn €4,50</h2>
-        <p  class="txt_w">een glas rode wijn van 0,25ml</p>
+        <p  class="txt_w">Een glas rode wijn van 0,25ml</p>
 
-        <h2>witte wijn €4,50</h2>
-        <p class="txt_w">een glas witte wijn van 0,25ml</p>
+        <h2>Witte wijn €4,50</h2>
+        <p class="txt_w">Een glas witte wijn van 0,25ml</p>
 
-        <h2>rosé wijn €4,50</h2>
-        <p class="txt_w">een glas rosé wijn van 0,25ml</p>
+        <h2>Rosé wijn €4,50</h2>
+        <p class="txt_w">Een glas rosé wijn van 0,25ml</p>
     </div>
 
 
     <div id="bieren">
-        <h2 class="txt_w">bieren</h2>
-        <h2>heineken €3,50</h2>
-        <p class="txt_w">een heineken van 0,40ml</p>
+        <h2 class="txt_w">Bieren</h2>
+        <h2>Heineken €3,50</h2>
+        <p class="txt_w">Een heineken van 0,40ml</p>
 
-        <h2>grolsch €3,50</h2>
-        <p class="txt_w">een grolsch van 0,40ml</p>
+        <h2>Grolsch €3,50</h2>
+        <p class="txt_w">Een grolsch van 0,40ml</p>
 
-        <h2>stella artois €3,50</h2>
-        <p class="txt_w">een stella artois van 0,40ml</p>
+        <h2>Stella artois €3,50</h2>
+        <p class="txt_w">Een stella artois van 0,40ml</p>
     </div>
 
     <div id="cocktails">
-        <h2 class="txt_w">cocktails €7,50</h2>
-        <h2>mojito</h2>
-        <p class="txt_w">een verfrissende cocktail met rum, munt, limoen en suiker</p>
-        <h2>pina colada</h2>
-        <p class="txt_w">een tropische cocktail met rum, kokos en ananas</p>
-        <h2>sex on the beach</h2>
-        <p class="txt_w">een zoete cocktail met vodka, perziklikeur, sinaasappel- en cranberrysap</p>
+        <h2 class="txt_w">Cocktails €7,50</h2>
+        <h2>Mojito</h2>
+        <p class="txt_w">Een verfrissende cocktail met rum, munt, limoen en suiker</p>
+        <h2>Pina colada</h2>
+        <p class="txt_w">Een tropische cocktail met rum, kokos en ananas</p>
+        <h2>Sex on the beach</h2>
+        <p class="txt_w">Een zoete cocktail met vodka, perziklikeur, sinaasappel- en cranberrysap</p>
     </div>
 
 
