01735e75624c80b6c61ac00258b8196fbab1737e
Jelle-490 <wiertzjelle@gmail.com>
2026-01-23 09:57:02 +0100

    bar.resrvations
    er stond een leeg reservation css bestand naast de gevulde dus die heb ik verwijderd en ik snacks toegevoegd aan de bar

--- web/bar.php
+++ web/bar.php
@@ -12,7 +12,7 @@
 </head>
 <body class="body">
     <div id="bar_mainIMG" class="mainIMG">
-     <img src="../assets/alcehol_img3.jpeg" height="20%" width="80%"  id="aldceholIMG" >
+     <img src="../assets/alcehol_img3.jpeg" width="80%"  id="aldceholIMG" >
     </div>
 
     <h1 class="txt" class='h1'>Dranken</h1>
@@ -24,68 +24,86 @@
       <div id="dranken" class="txt">
 
     <div id= "frisdranken">
-        <h2 class="txt_w">Frisdranken €3,20</h2>
-        <h2>Cola</h2>
-        <p class="txt_w">Een cola van 0,40ml</p>
+        <h2 class="txt_w">Frisdranken</h2>
+        <h2>Cola €3,20</h2>
+            <p class="txt_w">Een cola van 0,40ml</p>
 
-        <h2>Fanta</h2>
-        <p class="txt_w">Een fanta van 0,40ml</p>
+        <h2>Fanta €3,20</h2>
+            <p class="txt_w">Een fanta van 0,40ml</p>
 
-        <h2>7-up</h2>
-        <p class="txt_w">Een 7-up van 0,40ml</p>
+        <h2>7-up €3,20</h2>
+            <p class="txt_w">Een 7-up van 0,40ml</p>
 
-        <h2>Icetea</h2>
-        <p class="txt_w">Een icetea van 0,40ml</p>
+        <h2>Icetea €3,20</h2>
+            <p class="txt_w">Een icetea van 0,40ml</p>
         </div>
             
 
     <div id="wijnen">
-        <h2 class="txt_w">Wijnen €4,50</h2>
-        <h2>Rode wijn</h2>
-        <p  class="txt_w">Een glas rode wijn van 0,25ml</p>
+        <h2 class="txt_w">Wijnen</h2>
+        <h2>Rode wijn €4,50</h2>
+            <p  class="txt_w">Een glas rode wijn van 0,25ml</p>
 
-        <h2>Witte wijn</h2>
-        <p class="txt_w">Een glas witte wijn van 0,25ml</p>
+        <h2>Witte wijn €4,50</h2>
+            <p class="txt_w">Een glas witte wijn van 0,25ml</p>
 
-        <h2>Rosé wijn</h2>
-        <p class="txt_w">Een glas rosé wijn van 0,25ml</p>
+        <h2>Rosé wijn €4,50</h2>
+            <p class="txt_w">Een glas rosé wijn van 0,25ml</p>
     </div>
 
 
     <div id="bieren">
-        <h2 class="txt_w">Bieren €3,50</h2>
-        <h2>Heineken</h2>
-        <p class="txt_w">Een heineken van 0,40ml</p>
+        <h2 class="txt_w">Bieren </h2>
+        <h2>Heineken €3,50</h2>
+            <p class="txt_w">Een heineken van 0,40ml</p>
 
-        <h2>Grolsch</h2>
-        <p class="txt_w">Een grolsch van 0,40ml</p>
+        <h2>Grolsch €3,50</h2>
+            <p class="txt_w">Een grolsch van 0,40ml</p>
 
-        <h2>Stella artois </h2>
-        <p class="txt_w">Een stella artois van 0,40ml</p>
+        <h2>Stella artois €3,50</h2>
+            <p class="txt_w">Een stella artois van 0,40ml</p>
 
 
         <p class="txt_w">wij hebben ook een paar alcoholvrije biertje. Dat zou U tijdens het bestellen moeten vragen.</p>
     </div>
 
     <div id="cocktails">
-        <h2 class="txt_w">Cocktails €7,50</h2>
-        <h2>Mojito</h2>
-        <p class="txt_w">Een verfrissende cocktail met rum, munt, limoen en suiker</p>
-        <h2>Pina colada</h2>
-        <p class="txt_w">Een tropische cocktail met rum, kokos en ananas</p>
-        <h2>Sex on the beach</h2>
-        <p class="txt_w">Een zoete cocktail met vodka, perziklikeur, sinaasappel- en cranberrysap</p>
+        <h2 class="txt_w">Cocktails </h2>
+        <h2>Mojito €7,50</h2>
+            <p class="txt_w">Een verfrissende cocktail met rum, munt, limoen en suiker</p>
+        <h2>Pina colada €7,50</h2>
+            <p class="txt_w">Een tropische cocktail met rum, kokos en ananas</p>
+        <h2>Sex on the beach €7,50</h2>
+            <p class="txt_w">Een zoete cocktail met vodka, perziklikeur, sinaasappel- en cranberrysap</p>
     </div>
 
     <div id="mocktails">
-        <h2 class="txt_w">Mocktails €6,00</h2>
-            <h2>Virgin on the beach</h2>
-            <p class="txt_w">Een alcoholvrije versie van de klassieke sex on the beach cocktail</p>
-            <h2>No-jito</h2>
-            <p class="txt_w">Een alcoholvrije versie van de klassieke mojito cocktail</p>
-            <h2>Piña colada virgin</h2>
-            <p class="txt_w">Een alcoholvrije versie van de klassieke piña colada cocktail</p>
+        <h2 class="txt_w">Mocktails </h2>
+            <h2>Virgin on the beach €6,00</h2>
+                <p class="txt_w">Een alcoholvrije versie van de klassieke sex on the beach cocktail</p>
+            <h2>No-jito €6,00</h2>
+                <p class="txt_w">Een alcoholvrije versie van de klassieke mojito cocktail</p>
+            <h2>Piña colada virgin €6,00</h2>
+                <p class="txt_w">Een alcoholvrije versie van de klassieke piña colada cocktail</p>
 
+    </div>
+
+    <div id="snacks">
+        <h1 class="txt_w">Snacks</h1>
+        <h2>Chips and dip €6,50</h2>
+            <p class="txt_w">Tortilla chips met dip naar keuze (salsa, guacamole en queso)</p>
+
+        <h2>Nacho bowl €9,80</h2>
+            <p class="txt_w">Tortilla chips met pulled pork, gesmolten kaas, jalapeños, zure room en guacamole</p>
+
+        <h2>Gefrituur garnalen €9,50</h2>
+            <p class="txt_w">Gefrituurde garnalen met een citroen- en knoflooksaus</p>
+
+        <h2>Bitter ballen € 8,95 / € 16,75</h2>
+            <p class="txt_w">Bitterballen van 10 stuks of 20 stuks</p>
+        
+        <h2>Friet met mayonese €4,20</h2>
+            <p class="txt_w">Een medium friet met mayonese</p>
     </div>
       </div>
 
